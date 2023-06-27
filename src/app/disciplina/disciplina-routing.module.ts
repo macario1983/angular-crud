f63f95716eece6cfc5c99cad6957a058f9d4b8c4
Diego Macario <diego_macario@hotmail.com>
2023-06-27 14:32:20 -0300

    Criando a rota da disciplina

--- src/app/disciplina/disciplina-routing.module.ts
+++ src/app/disciplina/disciplina-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListDisciplinaComponent } from './list-disciplina/list-disciplina.component';
+import { FormDisciplinaComponent } from './form-disciplina/form-disciplina.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListDisciplinaComponent },
+  { path: 'add', component: FormDisciplinaComponent },
+  { path: 'edit', component: FormDisciplinaComponent },
+  { path: 'list', component: ListDisciplinaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
