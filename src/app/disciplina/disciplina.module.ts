b1974af536a72be4dbd0cde7baebd28a97fa08f2
Diego Macario <diego_macario@hotmail.com>
2023-06-27 14:30:04 -0300

    Criando o list da disciplina

--- src/app/disciplina/disciplina.module.ts
+++ src/app/disciplina/disciplina.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { DisciplinaRoutingModule } from './disciplina-routing.module';
 import { FormDisciplinaComponent } from '../aluno/form-disciplina/form-disciplina.component';
+import { ListDisciplinaComponent } from './list-disciplina/list-disciplina.component';
 
 
 @NgModule({
   declarations: [
-    FormDisciplinaComponent
+    FormDisciplinaComponent,
+    ListDisciplinaComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/disciplina/list-disciplina/list-disciplina.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-disciplina',
+  templateUrl: './list-disciplina.component.html',
+  styleUrls: ['./list-disciplina.component.css']
+})
+export class ListDisciplinaComponent {
+
+}
