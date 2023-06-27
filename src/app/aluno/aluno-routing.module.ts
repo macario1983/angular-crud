ed95dd081b3b047735daf0fbfce5aa46df78c5c3
Diego Macario <diego_macario@hotmail.com>
2023-06-27 12:01:31 -0300

    Setando as rotas do aluno

--- src/app/aluno/aluno-routing.module.ts
+++ src/app/aluno/aluno-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormAlunoComponent } from './form-aluno/form-aluno.component';
+import { ListAlunoComponent } from './list-aluno/list-aluno.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add', component: FormAlunoComponent },
+  { path: 'edit', component: FormAlunoComponent },
+  { path: 'list', component: ListAlunoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AlunoRoutingModule { }
+export class AlunoRoutingModule {}
