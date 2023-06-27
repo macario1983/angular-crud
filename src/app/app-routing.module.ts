9615e83fa8166c9a4009c3716a376fcb9b3acf3e
Diego Macario <diego_macario@hotmail.com>
2023-06-27 14:10:28 -0300

    Adicionando a rota filha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'aluno',
+    loadChildren: () =>
+      import('./aluno/aluno-routing.module').then((m) => m.AlunoRoutingModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
