ae16ff344130dbb1b8a4ccdaa65b3799c563c158
Diego Macario <diego_macario@hotmail.com>
2023-06-27 15:00:07 -0300

    Ajustando menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'home',
+    component: AppComponent
+  },
   {
     path: 'aluno',
     loadChildren: () =>
