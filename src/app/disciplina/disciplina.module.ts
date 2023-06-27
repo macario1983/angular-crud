1ad5a37cec2b4b4775c5e670de28b53900d66e67
Diego Macario <diego_macario@hotmail.com>
2023-06-27 14:27:51 -0300

    Criando o modulo da disciplina

---
+++ src/app/disciplina/disciplina.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DisciplinaRoutingModule } from './disciplina-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DisciplinaRoutingModule
+  ]
+})
+export class DisciplinaModule { }
