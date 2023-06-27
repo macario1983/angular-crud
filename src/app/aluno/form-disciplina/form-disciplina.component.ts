fdc42ca3d4c4129af34e78ad1af17bacd4a881ba
Diego Macario <diego_macario@hotmail.com>
2023-06-27 14:29:36 -0300

    Criando o form da disciplina

---
+++ src/app/aluno/form-disciplina/form-disciplina.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-disciplina',
+  templateUrl: './form-disciplina.component.html',
+  styleUrls: ['./form-disciplina.component.css']
+})
+export class FormDisciplinaComponent {
+
+}
--- src/app/disciplina/disciplina.module.ts
+++ src/app/disciplina/disciplina.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DisciplinaRoutingModule } from './disciplina-routing.module';
+import { FormDisciplinaComponent } from '../aluno/form-disciplina/form-disciplina.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FormDisciplinaComponent
+  ],
   imports: [
     CommonModule,
     DisciplinaRoutingModule
